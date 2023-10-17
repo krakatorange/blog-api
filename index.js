1585722fcece226a966fa92e19ec71daf8cd1508
Misha <mikhail.aleksandrov@nvc-inc.net>
2023-10-17 12:35:10 -0400

    removed tap tests

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 // index.js
-'use strict'
 const fastify = require('fastify')({ logger: true, ignoreTrailingSlash: true });
 const swaggerOptions = require('./config/swaggerOptions');
 const dbClient = require('./config/database');
 const posts = require('./routes/posts');
 require('dotenv').config();
 
-function build(opts={}) {
-    fastify.decorate('db', dbClient);
-    fastify.register(require('@fastify/swagger'), {});
-    fastify.register(require('@fastify/swagger-ui'), swaggerOptions);
-    const index = fastify.register(posts);
+const port = process.env.PORT || 3001;
 
-    return index
-}
+fastify.decorate('db', dbClient);
+fastify.register(require('@fastify/swagger'), {});
+fastify.register(require('@fastify/swagger-ui'), swaggerOptions);
+fastify.register(posts);
 
-module.exports = build
+fastify.listen({ port: port }, err => {
+    if (err) throw err;
+    fastify.log.info(`server listening on ${port}`);
+});
